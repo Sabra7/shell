0fea46cc39533bebb46c67516d25e6017e44c122
Mohammed Sabra <mo7amadoh@gmail.com>
2026-09-12 04:07:22 +0300

    Add files via upload

--- main.py
+++ main.py
@@ -1,33 +1,50 @@
 import sys
 import shutil
 import os
+import subprocess
+
+def execute_command(c):
+    for d in os.get_exec_path():
+        if os.access(fullpath := os.path.join(d, c), os.X_OK):
+            return fullpath
 
 def main():
     while True:
         sys.stdout.write("$ ")
-        pass
-        command = input()
+        sys.stdout.flush()
 
-        if command == "exit":
+        user_input = input()
+        if not user_input:
+            continue
+
+        command = user_input
+        cmd_name = command
+        file_name = cmd_name.split()[0]
+        tokens = cmd_name.split()
+
+        if cmd_name == "exit":
             break
 
         elif command.startswith("echo "):
             print(command[5:])
 
-        elif command.startswith("type "):
-            cmd_type = command[5:]
+        elif cmd_name.startswith("type "):
+            cmd_type = cmd_name[5:]
+
             if cmd_type in ["type","exit","echo"]:
                 print(f"{cmd_type} is a shell builtin")
+
             elif path := shutil.which(cmd_type):
                 print(f"{cmd_type} is {path}")
 
             else:
-                print(f"{command[5:]}: not found")
+                print(f"{cmd_name[5:]}: not found")
+
+        elif execute_command(file_name):
+            subprocess.run(tokens)
 
         else:
-            print(f"{command}: command not found")
-
-
+            print(f"{cmd_name}: command not found")
 
 
 
