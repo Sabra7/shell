e6dda5a74f95bb853046d73a1d5d7ad70aae065d
Mohammed Sabra <mo7amadoh@gmail.com>
2026-09-12 23:16:45 +0300

    Implement 'cd' command in main.py
    Added 'cd' command functionality to change directories.

--- main.py
+++ main.py
@@ -21,7 +21,8 @@ def main():
         tokens = user_input
         arg_w = user_input[1:]
         arg_t = " ".join(arg_w)
-        builtin = ["type","exit","echo","pwd"]
+        real_path = os.path.expanduser(arg_t)
+        builtin = ["type","exit","echo","pwd","cd"]
 # exit fun is done
 # TODO: use sys.exit(int(arg_t)) - handle empty arg
         if cmd_name == "exit": break
@@ -36,15 +37,24 @@ def main():
             elif path := shutil.which(arg_t): print(f"{arg_t} is {path}")
                 
             else: print(f"{arg_t}: not found")
-                
+
 # pwd function is working
         elif cmd_name == "pwd": print(os.getcwd())
+
+# cd function is working
+        elif cmd_name == "cd":
+            try:
+                os.chdir(real_path)
+            except FileNotFoundError:
+                print(f"cd: {real_path}: No such file or directory")
 
 # execute fun working< i think in windows isn't will work well
         elif execute_command(cmd_name): subprocess.run(tokens)
 
 # to get back ans let user know the command not found
         else: print(f"{cmd_name}: command not found")
+
+
 
 if __name__ == "__main__":
     main()
